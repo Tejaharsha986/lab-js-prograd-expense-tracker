279754391ccd1adda230a8b85bf925831a6a2c85
Teja <makkenabrothers143@gmail.com>
2022-02-22 18:01:32 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let balance = 0.00;
+let income = 0.00;
+let expense = 0.00;
+function addStroage(){
+    var text = document.getElementById("text").value;
+    var amount = parseFloat(document.getElementById("amount").value);
+    localStorage.setItem(text , amount);
+    document.getElementById("list").innerHTML += text+"-" +amount + '<br>' ;
 
+    if(amount > 0)
+    {
+        income += amount;
+        document.getElementById("money-plus").innerHTML="$"+income;
+    }
+    else{
+        expense+=amount
+        document.getElementById("money-minus").innerHTML="$"+expense;
+    }
+    balance = income + expense;
+    document.getElementById("balance").innerHTML="$"+balance
+}
